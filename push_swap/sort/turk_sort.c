53a99e942c1d94ea159715fb8364f7016269eb94
Marcio Silva <marcsilv@c2r10s2.42luanda.com>
2024-08-29 13:23:46 +0100

    turk_sort logic

--- push_swap/sort/turk_sort.c
+++ push_swap/sort/turk_sort.c
@@ -6,7 +6,7 @@
 /*   By: marcsilv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 12:30:57 by marcsilv          #+#    #+#             */
-/*   Updated: 2024/08/27 15:57:21 by marcsilv         ###   ########.fr       */
+/*   Updated: 2024/08/29 13:23:10 by marcsilv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,36 @@ void	turk_sort(t_stack *stack_a, t_stack *stack_b)
 {
 	int	primediff;
 	int	diff;
+	int	placeholder;
 	int	i;
 	int	k;
+	int	cost_up_counter;
 
 	push(stack_a, stack_b, "pa");	
 	push(stack_a, stack_b, "pa");	
-
+	
+	cost_up_counter = 0;
 	i = stack_b->top;
 	k = stack_b->top;
 	primediff = 0;
-	while (i >= -1)
+	while (i >= 0)
 	{
 		if (stack_a->collection[k] > stack_b->collection[i])
 			primediff = stack_a->collection[k] - stack_b->collection[i];
+		i--;
 		if (primediff != 0)
 			break;
-		i--;
 	}
-	while (i >= -1)
+	while (i >= 0)
 	{
 		if (stack_a->collection[k] > stack_b->collection[i])
 			diff = stack_a->collection[k] - stack_b->collection[i];
 		if (diff < primediff)
-			primediff = diff;
+			placeholder = i;
 		i--;
 	}
+	//cost_up
+	while (placeholder <= stack->collection[top])
+		cost_up_counter++;
 	return (0);
 }
